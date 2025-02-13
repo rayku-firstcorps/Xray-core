a22606aab4a8b1853e1ce2f8f1b83d8423cac87b
lmhMike <mike_just@163.com>
2025-02-13 16:30:53 +0800

    Update protocol.go

--- proxy/trojan/protocol.go
+++ proxy/trojan/protocol.go
@@ -11,8 +11,7 @@ import (
 )
 
 var (
-	//crlf = []byte{'\r', '\n'}
-	crlf = []byte{'\n', '\r'}
+	crlf = []byte{'\r', '\n'}
 
 	addrParser = protocol.NewAddressParser(
 		protocol.AddressFamilyByte(0x01, net.AddressFamilyIPv4),
